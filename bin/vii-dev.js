6491b4bfcf2d2551d66920c579098467db455fda
Mo Chen <mochen0505@hotmail.com>
2021-07-18 21:27:44 +0800

    feat: vii dev command modified

--- bin/vii-dev.js
+++ bin/vii-dev.js
@@ -1,12 +1,33 @@
 #!/usr/bin/env node
 
-const { SPEAR_PATH } = require('../scripts/constants')
+const program = require('commander');
+const chalk = require('chalk');
+const { SPEAR_PATH, SERVER_PORT } = require('../scripts/constants')
 const { resolve } = require('path')
 const storybook = require('@storybook/react/standalone')
+
+program
+  .usage('[options]')
+  .option('-p --port <port>', 'set storybook server port')
+
+program.on('--help', () => {
+  console.log(chalk.yellow('# run storybook with default port'));
+  console.log(chalk.white('# vii dev'));
+  console.log(chalk.yellow('# run storybook with custom port'));
+  console.log(chalk.white('# vii dev -p <port>'));
+})
+
+const args = require('minimist')(process.argv.slice(2));
+
+if (args.h || args.help) {
+  program.help()
+}
+
+const port = args.p || args.port || SERVER_PORT;
 
 storybook({
   mode: 'dev',
   configDir: resolve(SPEAR_PATH, './.storybook'),
-  port: 6006,
+  port: port,
   quiet: true
 })

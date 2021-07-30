f50b34a6bae13e3a3ab6cad666745104a45d53ee
Mo Chen <mochen0505@hotmail.com>
2021-07-31 01:05:19 +0800

    feat: 新增依赖

--- scripts/config/rollup.config.js
+++ scripts/config/rollup.config.js
@@ -1,16 +1,53 @@
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
-const commonjs = require('@rollup/plugin-commonjs');
-const { terser } = require('rollup-plugin-terser');
+const commonjs = require('@rollup/plugin-commonjs')
+const { terser } = require('rollup-plugin-terser')
+const filesize = require('rollup-plugin-filesize')
+const { babel } = require('@rollup/plugin-babel')
+const json = require('@rollup/plugin-json')
+const image = require('@rollup/plugin-image')
+const postcss = require('rollup-plugin-postcss')
 const { resolve } = require('path')
 const glob = require('glob')
 const chalk = require('chalk')
-const { PROJECT_PATH } = require('../constants')
+const { PROJECT_PATH, VENTI_PATH } = require('../constants')
 
 function setPlugins() {
   return [
     nodeResolve(),
+    json(),
+    image(),
+    postcss(),
+    // babel({
+    //   presets: [
+    //     [
+    //       `${VENTI_PATH}/node_modules/@babel/preset-env`,
+    //       {
+    //         modules: false,
+    //       },
+    //     ],
+    //     `${VENTI_PATH}/node_modules/@babel/preset-react`,
+    //     `${VENTI_PATH}/node_modules/@babel/preset-typescript`,
+    //   ],
+    //   plugins: [
+    //     [
+    //       `${VENTI_PATH}/node_modules/@babel/plugin-transform-runtime`,
+    //       {
+    //         corejs: {
+    //           version: 3,
+    //           proposals: true,
+    //         },
+    //         useESModules: true,
+    //       },
+    //     ],
+    //   ],
+    //   include: [
+    //     resolve(PROJECT_PATH, './packages'),
+    //   ],
+    //   babelHelpers: 'bundled'
+    // }),
     commonjs(),
-    terser()
+    terser(),
+    filesize()
   ]
 }
 
